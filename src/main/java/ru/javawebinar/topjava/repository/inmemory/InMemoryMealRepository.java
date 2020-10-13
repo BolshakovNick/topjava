4add5b14efb42617b5ea94ad68dda4310f67b2c0
Local <bolshakov_wrestling@icloud.com>
2020-10-13 21:04:50 +0300

    HW2 layers implementation

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,26 +1,36 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
+
     private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(this::save);
+        MealsUtil.meals.forEach(meal -> save(meal, 1));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Meal meal, int userId) {
+        log.info("save {}", meal);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
+            meal.setUserId(userId);
             repository.put(meal.getId(), meal);
             return meal;
         }
@@ -29,18 +39,23 @@ public class InMemoryMealRepository implements MealRepository {
     }
 
     @Override
-    public boolean delete(int id) {
+    public boolean delete(int id, int userId) {
+        log.info("delete {}", id);
         return repository.remove(id) != null;
     }
 
     @Override
-    public Meal get(int id) {
+    public Meal get(int id, int userId) throws NotFoundException {
+        log.info("get {}", id);
         return repository.get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public List<Meal> getAll(int userId) {
+        log.info("getAll");
+        return repository.values().stream()
+                .filter(meal -> meal.getUserId() == userId)
+                .sorted((o1, o2) -> o2.getDate().compareTo(o1.getDate()))
+                .collect(Collectors.toList());
     }
 }
-
--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,49 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.util.Collection;
+
+@Service
 public class MealService {
+    private final MealRepository repository;
 
-    private MealRepository repository;
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
 
+    public Meal create(Meal meal, int authUserId) {
+        return repository.save(meal, authUserId);
+    }
+
+    public void delete(int id, int authUserId) {
+        checkMealDoesNotBelongAuthUser(!repository.delete(id, authUserId), authUserId, id);
+    }
+
+    public Meal get(int id, int authUserId) {
+        Meal meal = repository.get(id, authUserId);
+        checkMealDoesNotBelongAuthUser(meal, authUserId, id);
+        return meal;
+    }
+
+    public Collection<Meal> getAll(int authUserId) {
+        return repository.getAll(authUserId);
+    }
+
+    public void update(Meal meal, int authUserId) {
+        checkMealDoesNotBelongAuthUser(repository.save(meal, authUserId), authUserId, meal.getId());
+    }
+
+    private void checkMealDoesNotBelongAuthUser(Meal meal, int authUserId, int mealId) {
+        checkMealDoesNotBelongAuthUser(meal == null, authUserId, mealId);
+    }
+
+    private void checkMealDoesNotBelongAuthUser(boolean expression, int authUserId, int mealId) {
+        if (expression) {
+            throw new NotFoundException("User " + authUserId + " doesn't have meal with id = " + mealId);
+        }
+    }
 }
