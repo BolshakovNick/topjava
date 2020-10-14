4a8424d5fc77beded66da2a5810ca4e1840b3672
Local <bolshakov_wrestling@icloud.com>
2020-10-14 14:17:40 +0300

    HW2 processing null date and time

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -9,10 +9,22 @@ public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
+        if (startTime == null) {
+            startTime = LocalTime.MIN;
+        }
+        if (endTime == null) {
+            endTime = LocalTime.MAX;
+        }
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
     }
 
     public static boolean isBetweenHalfOpen(LocalDateTime ldt, LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
+        if (startDate == null) {
+            startDate = LocalDate.MIN;
+        }
+        if (endDate == null) {
+            endDate = LocalDate.MAX;
+        }
         return ldt.toLocalDate().compareTo(startDate) >= 0 && ldt.toLocalDate().compareTo(endDate) < 0 && isBetweenHalfOpen(ldt.toLocalTime(), startTime, endTime);
     }
 
