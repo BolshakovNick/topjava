bbe066d5187660ce8e52c128799919876896ead6
Local <bolshakov_wrestling@icloud.com>
2020-10-20 21:16:16 +0300

    HW3 init table meals and JdbcMealRepository implementation

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,9 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (id, user_id, date_time, description, calories)
+VALUES (1, 100000, TIMESTAMP '2020-1-30 10:00', 'Завтрак', 500),
+       (2, 100000, TIMESTAMP '2020-1-30 13:00', 'Обед', 1000),
+       (3, 100001, TIMESTAMP '2015-6-1 14:00', 'Админ ланч', 510),
+       (4, 100001, TIMESTAMP '2015-6-1 14:00', 'Админ ужин', 1500);
